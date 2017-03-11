d52ce9ea462f3ae498d48c217749dbbd069568f3
https://github.com/tvykruta/gce_test.git <tomasvykruta@tomasv-macbook-air.local>
2017-03-11 10:59:15 -0500

    Added support for s simple user mutation to add new user.

--- apolloclient/src/components/User.js
+++ apolloclient/src/components/User.js
@@ -1,7 +1,6 @@
 "use strict";
 import React, { Component } from 'react'
 import { View, Text, TextInput } from 'react-native'
-import userQuery from '../queries/user.js';
 import styles from '../styles/user.js';
 
 
@@ -9,6 +8,7 @@ class User extends Component {
   constructor() {
     super();
     this.onUsernameChanged = this.onUsernameChanged.bind(this);
+    this.onAddUser = this.onAddUser.bind(this);
   }
   static propTypes = {
     data: React.PropTypes.shape({
@@ -16,13 +16,20 @@ class User extends Component {
       error: React.PropTypes.object,
       user: React.PropTypes.object,
     }).isRequired,
+    // mutations
+    addUser: React.PropTypes.func.isRequired,
   };
-
+  // Calls user changed redux action.
   onUsernameChanged(username) {
-      this.props.actions.actionUsernameChanged(username);
-      console.log(this.props);
+    this.props.actions.actionUsernameChanged(username);
+    console.log(this.props);
   }
-
+  // Calls add user mutation.
+  onAddUser(event) {
+    let username = event.nativeEvent.text;
+    let vars = {username:username, name:username, password:"test"};
+    this.props.addUser({variables: vars});
+  }
   recipes() {
       return Object.keys(this.props.searchedRecipes).map(
           key => this.props.searchedRecipes[key]);
@@ -49,6 +56,11 @@ class User extends Component {
           onChangeText={this.onUsernameChanged}
           style={styles.input} />
         <TextInput />
+        <Text style={{textAlign: 'center'}}>Add User</Text>
+        <TextInput
+          onEndEditing={this.onAddUser}
+          style={styles.input} />
+        <TextInput />
         <Text>Redux username_input is {this.props.users.username_input}</Text>
         <Text>Name: {this.props.data.user.name}</Text>
         <Text>Username: {this.props.data.user.username}</Text>
@@ -62,7 +74,6 @@ class User extends Component {
 // Redux binding starts here.
 //
 
-import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux';
 
 // Note: Coupling the Component's map function directly to the Redux state is bad, solution:
@@ -79,18 +90,21 @@ function mapStateToProps(state) {
 import { ActionCreators } from '../actions';
 // Note: It is possible to bind ActionCreators to other functions
 // besides Props. See http://stackoverflow.com/questions/34458261/how-to-get-simple-dispatch-from-this-props-using-connect-w-redux
+import { bindActionCreators } from 'redux'
 function mapDispatchToProps(dispatch) {
     // 'actions:' organizes callbacks into props.actions.<function_name>.
     return {actions: bindActionCreators(ActionCreators, dispatch)};
 }
 
+import {userQuery, addUserMutation} from '../queries/user.js';
 const userQueryOptions = {
       options: { variables: { name: "tvykruta" } }};
+// The mutation func will be accessilbe through props.addUser()
+const addUserMutationOptions = { name: "addUser" };
 
 import { graphql, compose } from 'react-apollo';
-
 export default compose(
   graphql(userQuery, userQueryOptions),
-//  graphql(mutation, mutationOptions),
+  graphql(addUserMutation, addUserMutationOptions),
   connect(mapStateToProps, mapDispatchToProps),
 )(User);
--- apolloclient/src/queries/user.js
+++ apolloclient/src/queries/user.js
@@ -2,12 +2,17 @@
 import gql from 'graphql-tag'
 
 
-const userQuery = gql`query UserQuery($name: String!){
-  user(username:$name)
-  {
+const userQuery = gql`query UserQuery($name: String!) {
+  user(username:$name) {
     name
     username
   }
 }`; 
 
-export default userQuery;
+const addUserMutation = gql`mutation AddUserMutation($username: String!, $name: String!, $password: String!) {
+  add_user(username:$username, name:$name, password:$password) {
+    name
+  }
+}`; 
+
+export { userQuery, addUserMutation };
