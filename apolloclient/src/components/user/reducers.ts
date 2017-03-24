b2760d3e558eea4025e92f7b08b2ef3a1a3e6795
lionhive <tvconsulting@gmail.com>
2017-03-23 21:57:01 -0400

    Minor cleanup.

--- apolloclient/src/components/user/reducers.ts
+++ apolloclient/src/components/user/reducers.ts
@@ -2,6 +2,9 @@
 import createReducer from "../../lib/createReducer";
 import * as types from "./action_types";
 
+// TODO: Use Action object and proper typscriptting look at
+// https://github.com/jaysoo/todomvc-redux-react-typescript
+
 // Reducers will show in a Component's this.props.users.<reducername>
 export const usernameInput = createReducer("tvykruta", {
     [types.USERNAME_CHANGED](state: any, action: any) {
--- apolloclient/src/scenes/login.tsx
+++ apolloclient/src/scenes/login.tsx
@@ -1,14 +1,12 @@
 "use strict";
 
 import React, { Component } from "react";
-import {
-  View,
-} from "react-native";
+import { View } from "react-native";
 import Login from "../components/login/container";
 
 class LoginScene extends Component<void, void> {
   public render() {
-    return (<Login/>);
+    return (<Login />);
   };
 }
 
